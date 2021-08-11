7395c4aa6b3e325030ad97bf3213d5a631323830
Zain Padela <zpadela@gmail.com>
2021-08-11 17:44:18 -0400

    Edited descriptions

--- 1-100/AddTwoNumbers.js
+++ 1-100/AddTwoNumbers.js
@@ -1,9 +1,6 @@
 /*
 
-  Definition for singly-linked list.
-  function ListNode(val, next) {
-    this.val = (val===undefined ? 0 : val)      this.next = (next===undefined ? null : next)
-  }
+ You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
 
 */
  var addTwoNumbers = function(l1, l2) {
