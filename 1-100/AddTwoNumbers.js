d263b947a890ec325ae66d8f4e5aa0533fbe20c3
Zain Padela <zpadela@gmail.com>
2021-08-11 17:29:11 -0400

    Add two numbers complete

---
+++ 1-100/AddTwoNumbers.js
@@ -0,0 +1,50 @@
+/*
+
+  Definition for singly-linked list.
+  function ListNode(val, next) {
+    this.val = (val===undefined ? 0 : val)      this.next = (next===undefined ? null : next)
+  }
+
+*/
+ var addTwoNumbers = function(l1, l2) {
+  var carry = 0;
+  var head = new ListNode(0);
+  var currentNode = head;
+
+  while (l1 || l2) {
+
+    var numOne = l1 ? l1.val : 0;
+    var numTwo  = l2 ? l2.val : 0;
+
+    var sum = numOne + numTwo + carry;
+    carry = 0;
+
+    if (sum > 9) {
+      sum = sum % 10;
+      carry = 1;
+    }
+
+    currentNode.next = new ListNode(sum);
+    currentNode = currentNode.next;
+
+
+    if (l1) {
+      l1 = l1.next;
+    };
+
+    if (l2) {
+      l2 = l2.next;
+    };
+  }
+
+  if (carry) {
+    currentNode.next = new ListNode(carry);
+  }
+
+  return head.next;
+};
+
+function ListNode(val, next) {
+  this.val = (val === undefined ? 0: val)
+  this.next = (next === undefined ? null: next)
+}
--- 1-100/Two Sum.js
+++ 1-100/Two Sum.js
@@ -1,11 +1,7 @@
 
 /*
 
-Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
-
-You can return the answer in any order.
+You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
 
 */
 
