f1b1c172c8f06558e924a80f6a5ce3dbb37a7d4b
Zain Padela <zpadela@gmail.com>
2021-10-27 15:41:13 -0400

    Completed RemoveElement

---
+++ 1-100/RemoveElement.js
@@ -0,0 +1,10 @@
+var removeElement = function(nums, val) {
+  for (var i = 0; i < nums.length; i++) {
+    if (nums[i] === val) {
+      nums.splice(i, 1)
+      i--
+    }
+  }
+  return console.log(nums)
+};
+
