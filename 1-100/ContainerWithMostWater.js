f406fe948ef5caad1fcdf6991fea4bd754e91eff
Zain Padela <zpadela@gmail.com>
2021-10-27 15:28:06 -0400

    Incomplete ContainerWithMostWater

---
+++ 1-100/ContainerWithMostWater.js
@@ -0,0 +1,30 @@
+
+var maxArea = function(height) {
+  var maxLength = height.length
+  var maxHeight = [0, 0];
+  var viableHeight = 0;
+  var viableLength = 0;
+
+  for (var i = 0; i < maxLength; i++) {
+
+    if (height[i] > maxHeight[0]) {
+      maxHeight = [height[i], i]
+    }
+
+    if (height[i] <= maxHeight[0] && height[i] > viableHeight && i - maxHeight[1] > viableHeight) {
+      viableHeight = height[i];
+      viableLength = i - maxHeight[1]
+    }
+  }
+
+  return (viableHeight * viableLength)
+};
+
+
+
+console.log(maxArea([1,8,6,2,5,4,8,3,7]) + ' Expected: 49')
+console.log(maxArea([1,1])  + ' Expected: 1')
+console.log(maxArea([4,3,2,1,4])  + ' Expected: 16')
+console.log(maxArea([1,2,1])  + ' Expected: 2')
+
+
