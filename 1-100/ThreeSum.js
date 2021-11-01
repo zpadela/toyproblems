ab37b60b0d0836d1f8077abd375bcfe8502434e2
Zain Padela <zpadela@gmail.com>
2021-11-01 15:30:34 -0400

    Completed 3Sum

---
+++ 1-100/ThreeSum.js
@@ -0,0 +1,35 @@
+var threeSum = function(nums) {
+  nums.sort((a,b) => a - b)
+  var triplets = [];
+
+  for (var i = 0; nums[i] <= 0; i++) {
+    var min = i + 1
+    var max = nums.length - 1
+
+    if (nums[i] === nums[i-1]){
+      continue;
+    }
+
+    while ( min < max) {
+      var sum  = nums[i] + nums[min] + nums[max];
+      if (sum < 0){
+        min++
+      } else if (sum > 0) {
+        max--
+      } else if (sum === 0) {
+        var triplet = [nums[i], nums[min], nums[max]];
+        triplets.push(triplet)
+          while (nums[min] === nums[min + 1]) {
+            min++
+          }
+        min++
+      }
+    }
+
+  }
+  return triplets;
+};
+
+
+console.log(threeSum([-1,0,1,2,-1,-4]))
+console.log(threeSum([0,0,0,0])) //expect [[-1,-1,2],[-1,0,1]]
