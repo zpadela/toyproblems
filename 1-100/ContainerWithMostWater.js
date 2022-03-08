ff72f55ee9b18ce9110fc096883af2fdb971eb82
Zain Padela <zpadela@gmail.com>
2022-03-08 15:07:49 -0500

    Completed ContainerWithMostWater

--- 1-100/ContainerWithMostWater.js
+++ 1-100/ContainerWithMostWater.js
@@ -1,32 +1,21 @@
-
 var maxArea = function(height) {
-  var maxLength = height.length
-  var maxHeight = [0, 0];
-  var viableHeight = 0;
-  var viableLength = 0;
-
-  for (var i = 0; i < maxLength; i++) {
-
-    if (height[i] > maxHeight[0]) {
-      maxHeight = [height[i], i]
-    }
-
-    if (height[i] <= maxHeight[0] && height[i] > viableHeight) {
-      viableHeight = height[i];
-    }
-
-    if (i - maxHeight > viableLength)
-
+  let start = 0;
+  let end = height.length - 1;
+  let area = 0;
+  while (start < end) {
+   let h = height[start] >= height[end] ? height[end] : height[start]
+   let l = end - start
+   let a = h * l
+   if (a > area) {
+       area = a;
+   }
+   if (height[start] > height[end]) {
+       end --
+   } else {
+       start++
+   }
   }
-
-  return (viableHeight * viableLength)
+  return area;
 };
 
-
-
-console.log(maxArea([1,8,6,2,5,4,8,3,7]) + ' Expected: 49')
-console.log(maxArea([1,1])  + ' Expected: 1')
-console.log(maxArea([4,3,2,1,4])  + ' Expected: 16')
-console.log(maxArea([1,2,1])  + ' Expected: 2')
-
-
+console.log(maxArea([1,8,6,2,5,4,8,3,7]));
